e9b0fd9987bea5cf5930a75494d94a9a8e77d625
Patricio <jpdibacco@gmail.com>
2020-11-29 19:10:59 +0100

    adding the sliders

--- main.py
+++ main.py
@@ -4,41 +4,36 @@ def buildUI():
   winWidth = 1024 # set a target width and reference this when you specify width
   if mc.window(winName, exists=True):
       mc.deleteUI(winName)
-  mc.window(winName, width=winWidth, title='Test Window')
-  # i always have keep a reference to the main columnLayout
+  mc.window(winName, width=winWidth, title='Forniture Generator')
+  #reference to the main columnLayout
   mainCL = mc.columnLayout() 
-  mc.text(label='text row 1')
+  mainRLWidth = [winWidth*0.6, winWidth*0.4]
+  mainRL = mc.rowLayout(w=winWidth, numberOfColumns=2, columnWidth2=mainRLWidth, rowAttach=(2, 'top', 0))
 
-  # tmpRowWidth controls the width of my columns in the rowLayout
-  # with reference to winWidth
-  tmpRowWidth = [winWidth*0.5, winWidth*0.5]
-  mc.rowLayout(numberOfColumns=2, columnWidth2=tmpRowWidth)
-  # at this point our UI pointer is under the rowLayout
-  mc.text(label='row column1', align='center', width=tmpRowWidth[0])
-  mc.button(label='column 2', width=tmpRowWidth[1])
-  # we've used up number of children components, if you add 1 more, 
-  # Maya will throw an error
-  # now to move our pointer back up the hierarchy, 
-  # which is our main columnLayout, mainCL
-  mc.setParent('..') # also can use mc.setParent(mainCL)
 
-  tmpRowWidth = [winWidth*0.5, winWidth*0.5]
-  mc.rowLayout(numberOfColumns=2, columnWidth2=tmpRowWidth)
-  mc.text(label='row column1', align='center', width=tmpRowWidth[0])
-  mc.button(label='column 2', width=tmpRowWidth[1])
-  mc.setParent('..')
+  mc.columnLayout(w=mainRLWidth[0]) # create a columnLayout under the first row of mainRL
+  mc.text(label='mainRL column 1', font='boldLabelFont')
+  mc.text(label='')
+#   mc.intSliderGrp( field=True, label='tablewidthz', minValue=4, maxValue=20, value=10, width=winWidth/2 )
+#   cmds.intSliderGrp( field=True, label='tableWidthx', minValue=2, maxValue=20, value=10, width=winWidth/2 )
+  slider1= cmds.intSliderGrp( field=True, label='tablewidthz', minValue=4, maxValue=20, value=10, width=winWidth/2 )
+  slider2= cmds.intSliderGrp( field=True, label='tableWidthx', minValue=2, maxValue=20, value=10, width=winWidth/2 )
+  slider3= cmds.intSliderGrp( field=True, label='tableHeight', minValue=2, maxValue=20, value=6, width=winWidth/2 )
+  slider4= cmds.intSliderGrp( field=True, label='Chaisewidthz', minValue=4, maxValue=6, value=4, width=winWidth/2 )
+  slider5= cmds.intSliderGrp( field=True, label='ChaiseWidthx', minValue=2, maxValue=6, value=5, width=winWidth/2 )
+  slider6= cmds.intSliderGrp( field=True, label='ChaiseHeight', minValue=2, maxValue=10, value=4, width=winWidth/2 )
+  slider7= cmds.intSliderGrp( field=True, label='Distance', minValue=-10, maxValue=10, value=-5, width=winWidth/2 )
+  mc.setParent('..') # this will exit the rowLayout back to the mainRL, same as mc.setParent(mainRL)
 
-  tmpRowWidth = [winWidth*0.7, winWidth*0.3]
-  mc.rowLayout(numberOfColumns=2, columnWidth2=tmpRowWidth)
-  mc.text(label='row column1', align='center', width=tmpRowWidth[0])
-  mc.button(label='column 2', width=tmpRowWidth[1])
-  mc.setParent('..')
+  mc.columnLayout(width=mainRLWidth[1]) # start another vertical layout
+  mc.text(label='mainRL column 2', font='boldLabelFont')
+  mc.text(label='')
+  mc.iconTextButton(style='iconAndTextVertical', image1='/home/fullarostaky/maya/2020/scripts/icons/Table_chaise_icon-01.png', label='Generate Table',width=mainRLWidth[1]*0.95)
+  #mc.button(label='button', width=mainRLWidth[1]*0.95, height=70)
 
-  mc.text(label='text row 3')
-
-  tmpWidth = [winWidth*0.3, winWidth*0.5, winWidth*0.2]
-  mc.textFieldButtonGrp(label='txt field', w=winWidth, columnWidth3=tmpWidth, buttonLabel='okay')
-  mc.button('full width button', width=winWidth)
+#   mc.setParent(mainCL) # set UI pointer back under the main columnLayout
+#   mc.text(label='')
+#   mc.button(label='full window width button', width=winWidth, height=40)
 
   mc.showWindow(winName)
   mc.window(winName, e=True, width=winWidth, height=1)
