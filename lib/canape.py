7283e542e96e1237f568edf71e1bfc04ccc94455
Patricio <jpdibacco@gmail.com>
2021-01-02 19:08:18 +0100

    fixed group issue

--- lib/canape.py
+++ lib/canape.py
@@ -1,7 +1,11 @@
 import maya.cmds as cmds
+import random
 sc = cmds.internalVar(userScriptDir=True)
 def runCanape():
 
+    moduleNames = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
+    mir = random.randint(0,19)
+    groupName = cmds.group(n = 'canape'+str(mir + mir), em=True)
     # default values:
     # module1_height = 4
     # module1_weigth = 10
@@ -13,33 +17,43 @@ def runCanape():
     module1_depth = cmds.intSliderGrp(slider3, q=True, value=True)
     module1_number = cmds.intSliderGrp(slider4, q=True, value=True)
 
-    cmds.group(n = 'canape', em=True)
-
     for i in range(module1_number):
-        cmds.polyCube(h=module1_height, w=module1_weigth, depth=module1_depth, n='module1_'+str(i))
+        mir = random.randint(0, 19)
+        crazyR = random.randint(0,1000)
+        rName = 'module1_'+ str(mir) + moduleNames[mir] + str(crazyR) + str(i) 
+        cmds.polyCube(h=module1_height, w=module1_weigth, depth=module1_depth, n=rName)
         cmds.move(i*module1_weigth/1.02,0,0)
         cmds.polyBevel3(offset=0.5)
         cmds.polySmooth(divisions=2)
-        cmds.parent('module1_'+str(i),'canape', relative=True)
+        cmds.parent(rName,groupName, relative=True)
         
 
     #cojines encima
 
     for i in range(module1_number):
-        cmds.polyCube(h=module1_height, w=module1_weigth, depth=module1_depth, n='module2_'+str(i))
+        mir = random.randint(0, 19)
+        crazyR = random.randint(0,1000)
+        rName = 'module2_'+ str(mir) + moduleNames[mir] + str(crazyR) + str(i) 
+        cmds.polyCube(h=module1_height, w=module1_weigth, depth=module1_depth, n=rName)
         cmds.rotate('73deg', 0, 0)
         cmds.move(i*module1_weigth/1.02,module1_depth/1.8,- module1_depth/1.5)
         cmds.polyBevel3(offset=0.5)
         cmds.polySmooth(divisions=2)
-        cmds.parent('module2_'+str(i),'canape', relative=True)
-        
-        
-    cmds.polyCube(h=(module1_height+module1_depth)/1.5, w=module1_height, depth=module1_depth*1.5, n='lateral1_'+str(i))
+        cmds.parent(rName,groupName, relative=True)
+
+    #laterales ? 
+
+    mir = random.randint(0, 19)
+    crazyR = random.randint(0,1000)
+    rName = 'lateral1_'+ str(mir) + moduleNames[mir] + str(crazyR) + str(i) 
+    cmds.polyCube(h=(module1_height+module1_depth)/1.5, w=module1_height, depth=module1_depth*1.5, n=rName)
     cmds.move(-module1_weigth/2.0,0,-0.5)
-    b=cmds.instance('lateral1_'+str(i))
+    b=cmds.instance(rName)
     cmds.move((module1_weigth*module1_number)-(module1_weigth/2.0),0,-0.5)
     cmds.polyBevel3(offset=0.5)
     cmds.polySmooth(divisions=2)
+    cmds.parent(rName,groupName, relative=True)
+    cmds.parent(b,groupName, relative=True)
 
 
 
